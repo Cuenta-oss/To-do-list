c386468c174e5f0031bea664407778f743a91972
Cuenta-oss <ejmenrique97@gmail.com>
2023-03-12 10:55:35 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const buttonCreateTask = document.querySelector(".task__create");
+const captureText = document.querySelector(".new__task");
+const showText = document.querySelector(".task");
+
+let idCounter = 0;
+
+
+function capturarTexto() {
+    idCounter++;
+    showText.innerHTML += `
+        <div class="container__task" id="${idCounter}">
+            <input type="checkbox">
+            <p class="task__text">${captureText.value}</p>
+            <img src="/img/trash3-fill.svg" alt="">
+        </div>`;
+    captureText.value = "";
+    actualizarEstados();
+};
+
+list.addEventListener('click', (event) => { 
+    if (event.srcElement.nodeName == "INPUT") {
+        actualizarEstados();
+    } else if (event.srcElement.nodeName == "IMG") {
+        console.log(event.srcElement.parentNode.id)
+        deleteTask(event.srcElement.parentNode.id);
+    }
+});
+
+function deleteTask(id) {
+    let idCapture = document.getElementById(id);
+    list.removeChild(idCapture);
+    actualizarEstados()
+}
+
+function actualizarEstados() {
+    let elemento = list.querySelectorAll("div");
+    let check = list.querySelectorAll("input[type='checkbox']:checked");
+    // console.log(elemento);
+    stats.innerHTML = `
+        <p class="task__state">Tareas pendientes: ${elemento.length}</p>
+        <p class="task__state">Tareas completas: ${check.length}</p> `;
+};
+
+// captureText.focus();
+buttonCreateTask.onclick = capturarTexto;
